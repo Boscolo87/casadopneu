b2beeb8a6da7f14b0dc9b066d1d0d447b4d57a9f
Boscolo87 <joelboscolo3@gmail.com>
2025-05-20 22:11:04 -0300

    projeto integrador 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Casa do Pneu - site carregado");
+// JavaScript do site Casa do Pneu
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("🚗 Casa do Pneu - site carregado com sucesso!");
+
+    const showPromotion = () => {
+      alert("Confira nossas promoções especiais de pneus e serviços! Entre em contato pelo WhatsApp.");
+    };
+  
+  
+  });
+  
